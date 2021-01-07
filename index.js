e79bd4bb74e7ba8d33935fda73f42756a705fd7d
Rogelio Lopez Bernal <rogrelio20052011@gmail.com>
2021-01-07 16:51:02 -0700

    plugins

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
+const { read } = require('fs');
 const readline = require('readline')
+const fs = require('fs');
+
 //Also made by RogelioLB
+
+const plug = [];
+
+function plugins(){
+  fs.readdir(__dirname+"/plugins",(err,file)=>{
+    file.forEach((f,i)=>{
+      if(f.endsWith(".js")){
+        plug.push({id:i,func: require(`./plugins/${f}`)});
+      }
+    })
+    console.log(plug);
+
+    console.log("\n\nForma de usar-- plug[<id>].func.(function)")
+  })
+}
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -12,27 +30,47 @@ function exitFunc(){
 }
 function Question(){
   rl.question('> ', (evaledParam) =>{
-    if (evaledParam == 'exit'){
-      return exitFunc()
+    try{
+      if (evaledParam == 'exit'){
+        return exitFunc()
+      }
+      if(evaledParam==="help"){
+        plugins();
+        return setImmediate(()=>Question());
+      }
+      let evaled = eval(evaledParam);
+      if(evaledParam.includes("console.log")||evaledParam.includes("console.dir")||evaledParam.includes("console.error")){
+        return setImmediate(()=>Question());
+      }else{
+        console.log(evaled);
+        setImmediate(()=>Question());
+      }
     }
-    let evaled = eval(evaledParam);
-    if(evaledParam.includes("console.log")||evaledParam.includes("console.dir")||evaledParam.includes("console.error")){
-      return setImmediate(()=>Question());
-    }else{
-      console.log(evaled);
-      setImmediate(()=>Question());
-    }
+      catch(err){
+        console.log("Error");
+        setImmediate(()=>Question());
+      }
   });
 }
 rl.question('> ', (evaledParam) =>{
+try{
   if (evaledParam == 'exit'){
     return exitFunc()
+  }
+  if(evaledParam==="help"){
+    plugins();
+    return setImmediate(()=>Question());
   }
   let evaled = eval(evaledParam);
   if(evaledParam.includes("console.log")||evaledParam.includes("console.dir")||evaledParam.includes("console.error")){
     return setImmediate(()=>Question());
   }else{
     console.log(evaled);
+    setImmediate(()=>Question());
+  }
+}
+  catch(err){
+    console.log("Error");
     setImmediate(()=>Question());
   }
 });
